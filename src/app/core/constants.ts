549b1ae6e8659101f576c115cbaa78bb854d7ef3
Nikhil Soni <nikhilbhoma@gmail.com>
2023-12-10 23:37:01 +0530

    added multi-color cards for better ui/ux

---
+++ src/app/core/constants.ts
@@ -0,0 +1,6 @@
+export const greenTypePokemonList: string[] = ['grass', 'poison'];
+export const blueTypePokemonList: string[] = ['water', 'ice'];
+export const redTypePokemonList: string[] = ['fighting', 'fire', 'dragon'];
+export const greyTypePokemonList: string[] = ['flying', 'bug', 'rock', 'steel', 'fairy'];
+export const darkTypePokemonList: string[] = ['dark', 'psychic', 'ghost'];
+export const yellowTypePokemonList: string[] = ['ground', 'normal', 'electric'];
--- src/app/modules/dashboard/component-pieces/pokemon-card/pokemon-card.component.ts
+++ src/app/modules/dashboard/component-pieces/pokemon-card/pokemon-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { PokemonDetailModel } from '../../models/pokemon.model';
+import { blueTypePokemonList, redTypePokemonList, greyTypePokemonList, darkTypePokemonList, yellowTypePokemonList } from '../../../../core/constants';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -17,6 +18,44 @@ export class PokemonCardComponent implements OnInit {
 
   public openPokemon(): void {
     this.clickEvent.emit(this.pokemon);
+  }
+
+  public getCardStyle(): any {
+    
+    if(!this.pokemon?.types?.length){
+      return {};
+    }
+
+    const type = this.pokemon?.types[0].type.name;
+    if(blueTypePokemonList.includes(type)){
+      return {
+        'background-color': '#58ABF6'
+      };
+    }
+
+    if(redTypePokemonList.includes(type)){
+      return {
+        'background-color': '#F7786B'
+      };
+    }
+    
+    if(yellowTypePokemonList.includes(type)){
+      return {
+        'background-color': '#FFCE4B'
+      };
+    }
+
+    if(greyTypePokemonList.includes(type)){
+      return {
+        'background-color': '#C0C0C0'
+      };
+    }
+
+    if(darkTypePokemonList.includes(type)){
+      return {
+        'background-color': '#7E57C2'
+      };
+    }
   }
 
 }
